40c3e2d509ca5cfc0a7e11457480ba6f924d4772
Debankur Saha <debankurrocks@gmail.com>
2025-05-26 16:30:08 +0530

    build from amd system

---
+++ buildinference.sh
@@ -0,0 +1,3 @@
+docker -f Dockerfileinference -t debankurs/xgbinference .
+docker tag debankurs/xgbinference:latest 531181936590.dkr.ecr.ap-south-1.amazonaws.com/debankurs/xgbinference:latest
+docker push 531181936590.dkr.ecr.ap-south-1.amazonaws.com/debankurs/xgbinference:latest
